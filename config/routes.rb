b95f930473277985d344f0b6b58c1834c9242210
keunyoung kim <topluv11@gmail.com>
2017-03-31 19:20:23 +0000

    route, readme modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'peachter/index'
-
+  get 'peachter/create'
   get 'home/index'
   get 'apeachbook/index'
+  get 'apeachbook/create'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
